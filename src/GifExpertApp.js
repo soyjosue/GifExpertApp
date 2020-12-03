9f588d46c94514eafad8f709c1a7005b5852b55c
Elvis Inoa <josueinoa02@gmail.com>
2020-12-02 22:50:37 -0500

    Terminamos Componente AddCategory

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import AddCategory from './components/AddCategory';
 
 const GifExpertApp = () => {
+
+  const [ categories, setCategories ] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
 
   return (
     <>
       <h2>GifExpertApp</h2>
-      <hr />
+      <AddCategory setCategories={setCategories} />
+      <hr /> 
+
+      <ol>
+        {
+          categories.map( category => {
+            return <li key={ category }> { category } </li>
+          } )
+        }
+      </ol>
     </>
   )
 }
